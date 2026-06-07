e86288a713a7a7535aa0f01c05de87d00a689300
Kajal chauhan <kajalchauhan798371@gmail.com>
2026-06-07 22:26:47 +0530

    More traigle pattern of character

--- all_pattern.cpp
+++ all_pattern.cpp
@@ -1,31 +1,99 @@
 #include <iostream>
 using namespace std;
 void pattern1(int row)
-{ int start=1;
-    for (int i=0;i<row;i++){
-        if(i%2==0){
-            start=0;
+{
+    int start = 1;
+    for (int i = 0; i < row; i++)
+    {
+        if (i % 2 == 0)
+        {
+            start = 0;
         }
-        else start=1;
-        for(int j=0;j<i;j++){
-cout<<start;
-start=1-start;
+        else
+            start = 1;
+        for (int j = 0; j < i; j++)
+        {
+            cout << start;
+            start = 1 - start;
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 void pattern2(int row)
-{ int start=1;
-    for (int i=0;i<row;i++){
-        if(i%2==0){
-            start=0;
+{
+    int start = 1;
+    for (int i = 0; i < row; i++)
+    {
+        if (i % 2 == 0)
+        {
+            start = 0;
         }
-        else start=1;
-        for(int j=0;j<i;j++){
-cout<<start;
-start=1-start;
+        else
+            start = 1;
+        for (int j = 0; j < i; j++)
+        {
+            cout << start;
+            start = 1 - start;
         }
-        cout<<endl;
+        cout << endl;
+    }
+}
+void pattern3(int row)
+{
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        for (int j = 1; j <= 2 * (row - i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+}
+void pattern4(int row)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (char j = 'A'; j <= 'A' + i; j++)
+        {
+            cout << j;
+        }
+        
+        cout << endl;
+    }
+}
+void pattern5(int row)
+{
+    for (int i = 1; i <= row; i++)
+    {
+        for (char j = 'A'; j <= 'A'+row -i; j++)
+        {
+            cout << j;
+        }
+        
+        cout << endl;
+    }
+}
+void pattern6(int row)
+{ 
+    for (int i = 0; i < row; i++)
+    {
+        char c= 'A' + i;
+        for (char j = 'A'; j <= 'A' + i; j++)
+        { 
+           
+            cout << c ;
+        }
+        
+        cout << endl;
     }
 }
 int main()
@@ -34,5 +102,5 @@ int main()
     int row;
     cout << "enter the number of rows";
     cin >> row;
-    pattern2(row);
+    pattern6(row);
 }
